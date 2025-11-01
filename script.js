24c222fc3c4428297ae4522a36b3a40bbc71c252
Vedant Kherade <152008775+vedantkherade@users.noreply.github.com>
2025-11-01 22:58:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
   //your code here
+	let res = [];
+	for(let i=0; i<arr.length; i++){
+		let val = arr[i];
+		val++;
+		res.push(val);
+	}
+	return res;
 }
-
+ 
 alert(incrementArray(arr));
